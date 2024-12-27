e3d5530d9b5ab498ed5e8eab971a5cf24bfff99c
xltan23 <tanxianliang23@gmail.com>
2024-12-27 19:31:26 +0800

    test commit

--- vttp/d1/Operator.java
+++ vttp/d1/Operator.java
@@ -8,32 +8,32 @@ public class Operator {
     public static void main(String[] args) {
         Console cons = System.console();
         Float result = 0f; 
-        String operator = cons.readLine("Choose an operator: add, sub, mul, div\n");
+        String operator = cons.readLine("Choose an operator: add, subtract, multiply, divide\n");
         String opnd0 = cons.readLine("Choose 1st number: ");
         String opnd1 = cons.readLine("Choose 2nd number: ");
 
         int val0 = Integer.parseInt(opnd0);
         int val1 = Integer.parseInt(opnd1);
+        String opSymbol = ("");
 
         switch(operator) {
             case "add":
                 result = (float)(val0 + val1);
+                opSymbol = "+";
                 break;
-            case "sub":
+            case "subtract":
                 result = (float)(val0 - val1);
+                opSymbol = "-";
                 break;
-            case "mul":
+            case "multiply":
                 result = (float)(val0 * val1);
+                opSymbol = "x";
                 break;
-            case "div":
+            case "divide":
                 result = (float)(val0/val1);
+                opSymbol = "/";
             }
 
-            System.out.printf("Result: %d %s %d = %.3f", val0, operator, val1, result);
-
-            default:
+            System.out.printf("Result: %d %s %d = %.3f", val0, opSymbol, val1, result);
      }
-
-        
-
 }
